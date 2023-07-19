4220be59f77fd2581d0b9f5529101f0864e65f5c
Krushna <krushna.coreaiminds@outlook.com>
2023-07-19 23:10:37 +0530

    Practice pUrpose

--- 16_3.c
+++ 16_3.c
@@ -4,7 +4,8 @@
 
 int Asum(int brr[])
 {
-    int i=0,sum=0;   
+    int i=0,sum=0;
+
     for(i=0;i<5;i++)
     {
         sum+=brr[i];
@@ -16,7 +17,7 @@ int Asum(int brr[])
 int main()
 {
     int arr[] = {82,35,2,567,68};
-  
+
     int r = Asum(&arr[0]);
     printf("\n");
     printf("arr = %d\n",arr);
